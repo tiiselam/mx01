9f6649a9ac91f7fde97619a863cca034b3eca36c
jcfernandezz <jcfernandezz@gmail.com>
2018-09-14 19:43:56 -0400

    Corrige bug. Los datos de la cuenta origen y destino deben aparecer de acuerdo al catálogo de formas de pago del sat

--- sql/125_fCfdiDocumentoDePago.function.sql
+++ sql/125_fCfdiDocumentoDePago.function.sql
@@ -3,12 +3,14 @@ IF OBJECT_ID ('dbo.fCfdiDocumentoDePago') IS NOT NULL
 GO
 
 
-CREATE function [dbo].fCfdiDocumentoDePago (@RMDTYPAL smallint, @DOCNUMBR varchar(21))
+create function [dbo].fCfdiDocumentoDePago (@RMDTYPAL smallint, @DOCNUMBR varchar(21))
 returns table 
 as
 --Propósito. Devuelve datos de un cobro
 --Requisitos. Obtiene la tc del pago que permita que el monto del pago sea siempre mayor en casos especiales. Ver fCfdiDocumentoDePagoRelacionado.
+--			El catálogo del sat debe estar configurado en el tipo FRPGB para cada forma de pago bancarizada
 --30/11/17 jcf Creación cfdi
+--14/09/18 jcf Agrega flags para incluir o no incluir campos de acuerdo al catálogo del sat: c_formaPago
 --
 return
 		(
@@ -30,22 +32,22 @@ return
 			end											FormaDePagoP,
  			LTRIM(RTRIM(C.ISOCURRC))					MonedaP,
 			pago.TipoCambioP,
-
 			hdr.ororgtrx								Monto,
-			CASE when hdr.cheknmbr = '' then null 
-				else rtrim(hdr.cheknmbr) 
-			end											NumOperacion,
-			cp.param1									RfcEmisorCtaOrd,
-			cp.param2									NomBancoOrdExt,
-			cp.param3									CtaOrdenante,
-			tef.TXRGNNUM								RfcEmisorCtaBen, 
-			tef.EFTBANKACCT								CtaBeneficiario
+
+			case when substring(isnull(flagsPagos.descripcion, '00000000'), 2, 1) = '1' then rtrim(hdr.cheknmbr) else null 	end									NumOperacion,
+			case when cp.param1 NOT like 'no existe tag%' and substring(isnull(flagsPagos.descripcion, '00000000'), 3, 1) = '1' then cp.param1 else null end	RfcEmisorCtaOrd,
+			case when cp.param2 not like 'no existe tag%' and substring(isnull(flagsPagos.descripcion, '00000000'), 8, 1) = '1' then cp.param2 else null end	NomBancoOrdExt,
+			case when cp.param3 not like 'no existe tag%' and substring(isnull(flagsPagos.descripcion, '00000000'), 4, 1) = '1' then cp.param3 else null end	CtaOrdenante,
+			case when substring(isnull(flagsPagos.descripcion, '00000000'), 5, 1) = '1' then rtrim(tef.TXRGNNUM) else null end									RfcEmisorCtaBen,
+			case when substring(isnull(flagsPagos.descripcion, '00000000'), 6, 1) = '1' then rtrim(tef.EFTBANKACCT) else null end								CtaBeneficiario
+
 		FROM dbo.vwRmTransaccionesTodas hdr
  			left join dynamics.dbo.MC40200 c on c.CURNCYID = HDR.CURNCYID
 			outer apply dbo.fCfdiMcpFormaPago(hdr.DOCNUMBR) mcp
 			left join CM00101 tef 
 				on tef.CHEKBKID = CASE WHEN hdr.bchsourc like '%MCP%' then mcp.tii_chekbkid else hdr.mscschid end
 			outer apply dbo.fCfdiFormaPagoManual(hdr.mscschid, hdr.CSHRCTYP, hdr.FRTSCHID) ch
+			outer apply dbo.fCfdiCatalogoGetDescripcion('FRPGB', CASE WHEN hdr.bchsourc like '%MCP%' then Rtrim(mcp.grupid) else ch.FormaPago end) flagsPagos
 			outer apply dbo.fCfdiParametrosCliente(hdr.custnmbr, 'RfcEmisorCtaOrd', 'NomBancoOrdExt', 'CtaOrdenante', 'NA', 'NA', 'NA', 'PREDETERMINADO') cp
 			outer apply (select max(TipoCambioP) TipoCambioP from dbo.fCfdiDocumentoDePagoRelacionado(hdr.RMDTYPAL, hdr.docnumbr)) pago
 			outer apply dbo.fcfdiparametros('MEXTZONE','NA','NA','NA','NA','NA','PREDETERMINADO') pa
--- sql/126_fCfdiDocumentoDePagoXMLPago.function.sql
+++ sql/126_fCfdiDocumentoDePagoXMLPago.function.sql
@@ -3,13 +3,14 @@ IF OBJECT_ID ('dbo.fCfdiDocumentoDePagoXMLPago') IS NOT NULL
 GO
 
 
-CREATE function [dbo].fCfdiDocumentoDePagoXMLPago (@RMDTYPAL smallint, @DOCNUMBR varchar(21))
+create function [dbo].fCfdiDocumentoDePagoXMLPago (@RMDTYPAL smallint, @DOCNUMBR varchar(21))
 returns xml 
 as
 --Propósito. DEVOLVER UN XML PARA UN COBRO TOTALMENTE APLICADO Y CONTABILIZADO, DE OTRO MODO DEVOLVER NULL
---Requisitos. EL COBRO DEBE ESTAR TOTALMENTE APLICADO Y CONTABILIZADO
+--Requisitos. EL COBRO DEBE ESTAR TOTALMENTE APLICADO Y CONTABILIZADO. 
 --24/10/17 lt Creación cfdi
 --10/11/17 jcf Correcciones varias
+--14/09/18 jcf Ajuste debido a cambios en fCfdiDocumentoDePago. Campos de pago de la cuenta origen y destino
 --
 begin
 	declare @cnp xml;
@@ -24,12 +25,14 @@ begin
  			hdr.MonedaP '@MonedaP',
 			cast(hdr.TipoCambioP as numeric(19,6)) '@TipoCambioP',
 			cast(hdr.Monto as numeric(19,2)) '@Monto',
-			hdr.NumOperacion '@NumOperacion',
-			case when hdr.RfcEmisorCtaOrd like 'no existe tag%' then null else hdr.RfcEmisorCtaOrd end '@RfcEmisorCtaOrd',
-			case when hdr.NomBancoOrdExt like 'no existe tag%' then null else hdr.NomBancoOrdExt  end '@NomBancoOrdExt',
-			case when hdr.CtaOrdenante like 'no existe tag%' then null else hdr.CtaOrdenante  end '@CtaOrdenante',
-			rtrim(hdr.RfcEmisorCtaBen) '@RfcEmisorCtaBen', 
+
+			CASE when rtrim(hdr.NumOperacion) != '' then rtrim(hdr.NumOperacion) else null end '@NumOperacion',
+			hdr.RfcEmisorCtaOrd '@RfcEmisorCtaOrd',
+			hdr.NomBancoOrdExt '@NomBancoOrdExt',
+			hdr.CtaOrdenante '@CtaOrdenante',
+			rtrim(hdr.RfcEmisorCtaBen) '@RfcEmisorCtaBen',
 			rtrim(hdr.CtaBeneficiario) '@CtaBeneficiario',
+
 			[dbo].[fCfdiDocumentoDePagoXML_Nodo_Relacionado] (hdr.RMDTYPAL, hdr.DOCNUMBR)
 		FROM dbo.fCfdiDocumentoDePago(@RMDTYPAL, @DOCNUMBR) hdr
 		where hdr.docnumbr = @DOCNUMBR	
@@ -44,3 +47,5 @@ go
 IF (@@Error = 0) PRINT 'Creación exitosa de: [fCfdiDocumentoDePagoXMLPago]()'
 ELSE PRINT 'Error en la creación de: [fCfdiDocumentoDePagoXMLPago]()'
 GO
+
+
