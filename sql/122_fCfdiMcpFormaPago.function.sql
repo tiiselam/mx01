9a28419a331b8415fbabdcb0814c596562655b5e
jc fernandezz <jcfernandezz@gmail.com>
2018-12-18 20:10:26 -0400

    ajusta scripts para instalador

--- sql/122_fCfdiMcpFormaPago.function.sql
+++ sql/122_fCfdiMcpFormaPago.function.sql
@@ -27,6 +27,44 @@ begin
 	) on [PRIMARY];
 end
 
+IF not EXISTS (SELECT 1 FROM dbo.sysobjects WHERE id = OBJECT_ID(N'dbo.nfMCP_PM20100') AND OBJECTPROPERTY(id,N'IsTable') = 1)
+begin
+	CREATE TABLE [dbo].[nfMCP_PM20100](
+		[MCPTYPID] [char](21) NOT NULL,
+		[NUMBERIE] [char](21) NOT NULL,
+		[MEDIOID] [char](21) NOT NULL,
+		[BANKID] [char](15) NOT NULL,
+		[DOCNUMBR] [char](21) NOT NULL,
+		[TITACCT] [char](65) NOT NULL,
+		[EMIDATE] [datetime] NOT NULL,
+		[DUEDATE] [datetime] NOT NULL,
+		[LINEAMNT] [numeric](19, 5) NOT NULL,
+		[AMOUNTO] [numeric](19, 5) NOT NULL,
+		[CURNCYID] [char](15) NOT NULL,
+		[STSDESCR] [char](31) NOT NULL,
+		[LNSEQNBR] [numeric](19, 5) NOT NULL,
+		[CHEKBKID] [char](15) NOT NULL,
+		[BANACTID] [char](21) NOT NULL,
+		[CURRNIDX] [smallint] NOT NULL,
+		[TII_MCP_Realized_Gain_Lo] [numeric](19, 5) NOT NULL,
+		[TII_MCP_Clearing] [smallint] NOT NULL,
+		[TII_MCP_Checkbook_Integ] [tinyint] NOT NULL,
+		[TII_MCP_Integrated_Date] [datetime] NOT NULL,
+		[TII_CHEKBKID] [char](15) NOT NULL,
+		[VOIDED] [tinyint] NOT NULL,
+		[VOIDDATE] [datetime] NOT NULL,
+		[TXRGNNUM] [char](25) NOT NULL,
+		[DEX_ROW_ID] [int] IDENTITY(1,1) NOT NULL,
+	 CONSTRAINT [PKnfMCP_PM20100] PRIMARY KEY NONCLUSTERED 
+	(
+		[MCPTYPID] ASC,
+		[NUMBERIE] ASC,
+		[MEDIOID] ASC,
+		[LNSEQNBR] ASC
+	)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+	) ON [PRIMARY]
+end
+
 -----------------------------------------------------------------------------------------------------------------------------
 IF not EXISTS (SELECT 1 FROM dbo.sysobjects WHERE id = OBJECT_ID(N'dbo.nfmcp30100') AND OBJECTPROPERTY(id,N'IsTable') = 1)
 begin
